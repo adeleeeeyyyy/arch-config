dcd716e5e995504cb873f55221dbd472a6d543e5
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-05-22 20:28:55 +0700

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+# ~/.config/fish/config.fish
+
+clear
+alias sistem="sudo systemctl"
+alias apdet="sudo pacman -Syu"
+alias ambil="git clone"
+alias panen="npm i"
+alias mulai="php artisan serve"
+alias migret="php artisan migrate"
+alias pres="php artisan migrate:fresh"
+alias kon="php artisan make:controller"
+alias model="php artisan make:model"
+alias meki="php artisan make:migration"
+alias donlot="sudo pacman -Sy"
+alias sail="./vendor/bin/sail"
+alias larapel="composer create-project laravel/laravel"
+fastfetch
+
